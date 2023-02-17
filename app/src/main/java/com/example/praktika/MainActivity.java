09275ed3865dafd12bf58aea21191337d7d87f52
OlgaGurdzhi <olgagurdzhi@gmail.com>
2023-02-17 22:40:01 +0300

    Toast

--- app/src/main/java/com/example/praktika/MainActivity.java
+++ app/src/main/java/com/example/praktika/MainActivity.java
@@ -1,13 +1,59 @@
 package com.example.praktika;
+
+
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MyApp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(),"On Create",Toast.LENGTH_SHORT).show();
+        Log.i(TAG, "Created");
+
+
+    }
+
+    @Override
+    protected void onStop() {
+        Toast.makeText(getApplicationContext(),"On stop Toast",Toast.LENGTH_SHORT).show();
+        Log.w(TAG,"ВНИМАНИЕ!");
+        if (isFinishing()){
+            finish();
+        }
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Toast.makeText(getApplicationContext(),"On Destroy",Toast.LENGTH_SHORT).show();
+        Log.v(TAG,"Destroy is the off of programm....");
+        super.onDestroy();
+        finish();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.wtf(TAG, "Lol, i dont know what happened");
+        Toast.makeText(getApplicationContext(),"On Pause",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.wtf(TAG,"wtf");
+        Log.d(TAG, "Debug msg");
+        Toast.makeText(getApplicationContext(),"On Resume",Toast.LENGTH_SHORT).show();
     }
 }
